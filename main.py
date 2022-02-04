2e14293ceed30afe16c8b8e3b44079ff2175447d
Stanislav <69276462+stasgoodman@users.noreply.github.com>
2022-02-04 17:15:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from utils.configs_reader import DataReader
-
+from utils.catapi_request import GotoApi
 
 data_reader = DataReader()
 print(data_reader.host, data_reader.auth_value)
+
+go_cat_api = GotoApi()
+resp = go_cat_api.get_votes()
+print(resp.status_code, resp.url, resp.reason)
