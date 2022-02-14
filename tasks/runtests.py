2e534efd4e72660e78cb9e9765b4513b7cc97553
Stanislav <69276462+stasgoodman@users.noreply.github.com>
2022-02-14 22:23:37 +0200

    Update runtests.py

--- tasks/runtests.py
+++ tasks/runtests.py
@@ -1,7 +1,7 @@
+from tests.base import *
 from tests.test_catapi import *
 
 
 def run_catapi_tests():
-    test_suite = unittest.TestLoader().loadTestsFromTestCase(TestCatApi)
+    test_suite = unittest.TestLoader().loadTestsFromTestCase(Test1stCatApi)
     unittest.TextTestRunner(verbosity=2).run(test_suite)
-
